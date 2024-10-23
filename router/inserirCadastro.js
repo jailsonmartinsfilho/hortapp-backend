53102487d3baf2d6ec706005638ba5ce86f7ebb4
jailsonmartinsfilho <jajaraiki@gmail.com>
2024-10-23 16:25:14 -0300

    wawa

--- router/inserirCadastro.js
+++ router/inserirCadastro.js
@@ -3,15 +3,15 @@ const router = Router();
 const connection = require('../database.js');
 
 router.post('/inserirCadastro', (req, res) => {
-    const {nome, email, senha} = req.body;
-    const dadosUsuario = {nome, email, senha};
+    const { nome, email, senha } = req.body;
+    const dadosUsuario = { nome, email, senha };
 
     connection.query('INSERT INTO usuarios SET ?', dadosUsuario, (err, result) => {
         if (err) {
             console.error('Erro ao inserir dados:', err);
-            res.status(500).send('Erro ao inserir dados no banco de dados');
+            res.sendStatus(500);
         } else {
-            res.send('Cadastro realizado');
+            res.sendStatus(200); 
         }
     });
 });
